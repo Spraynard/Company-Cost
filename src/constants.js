60f167d098f1f6ac440bce807ee04c2c1f5f1aaa
Spraynard <kellan.martin@gmail.com>
2018-07-29 23:56:30 -0400

    Changing some stuff around. Starting to work on implementing editing

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,17 @@
 // These are action types that will be used within the app
 const constants = {
+	/* Expense group action constant */
 	ADD_EXPENSE_GROUP : "ADD_EXPENSE_GROUP",
-	EDIT_EXPENSE_GROUP : "EDIT_EXPENSE_GROUP",
-	SAVE_EXPENSE_GROUP : "SAVE_EXPENSE_GROUP",
 	REMOVE_EXPENSE_GROUP : "REMOVE_EXPENSE_GROUP",
-	/* Children of expense groups */
+
+	/* Children of expense groups action constants */
 	ADD_EXPENSE_GROUP_CHILD : "ADD_EXPENSE_GROUP_CHILD",
-	EDIT_EXPENSE_GROUP_CHILD : "EDIT_EXPENSE_GROUP_CHILD",
-	SAVE_EXPENSE_GROUP_CHILD : "SAVE_EXPENSE_GROUP_CHILD",
 	REMOVE_EXPENSE_GROUP_CHILD : "REMOVE_EXPENSE_GROUP_CHILD",
+
+	/* Constants used for item editing */
+	EDIT_ENTITY : "EDIT_ENTITY",
+	SAVE_ENTITY : "SAVE_ENTITY",
+	ENTITY_CANCEL_EDIT : "ENTITY_CANCEL_EDIT"
 };
 
 module.exports = constants;
