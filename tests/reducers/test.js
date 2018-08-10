a080fa6277abb5c3293408538c9144fc1a913d7f
Spraynard <kellan.martin@gmail.com>
2018-08-09 21:22:22 -0400

    Making headway on real life testing of reducers

--- tests/reducers/test.js
+++ tests/reducers/test.js
@@ -1,10 +1,16 @@
 import C from "../../src/constants";
 
+/**
+ * Expense_Group specific reducers
+ */
 import {
 	expense_groups,
 	expense_group_by_id,
 } from "../../src/reducers/expense_group_reducers";
 
+/**
+ * Expense Group Child specific reducers
+ */
 import {
 	expense_group_children,
 	expense_group_child_by_id,
@@ -19,6 +25,16 @@ import {
 	removeExpenseGroupChild
 } from "../../src/actions";
 
+var expense_group_child_action_helper = {};
+
+beforeAll(() => {
+	expense_group_child_action_helper.title = "Test";
+	expense_group_child_action_helper.description = "A Test Expense Group Child";
+	expense_group_child_action_helper.cost = 1.00;
+	expense_group_child_action_helper.costUOM = "hour";
+	expense_group_child_action_helper.parentID = 1;
+});
+
 describe("Expense Groups", () => {
 	var state;
 
@@ -26,10 +42,11 @@ describe("Expense Groups", () => {
 		state = [];
 	});
 
+	// Not using an action generator with this test
 	test("Will return initial state if there is no type given.", () => {
-		var action = {
-			id : 0
-		};
+		var action = addExpenseGroup({ title : "Test Title" });
+		// Casting the type as undefined in order to test the behavior of this test.
+		action.type = undefined;
 
 		var results = expense_groups( state, action );
 
@@ -37,27 +54,23 @@ describe("Expense Groups", () => {
 	});
 
 	test("Will add an expense group ID to the state array", () => {
-		var action = {
-			type : C.ADD_EXPENSE_GROUP,
-			id : 0
-		};
 
+		var title = "Expense Group Test #1";
+		var action = addExpenseGroup({ title });
 		var results = expense_groups( state, action );
 
-		expect( results ).toEqual([ 0 ]);
+		// expect( results ).toEqual([ 0 ]); // Expect results to output an array
+		expect(results[0].length).toBe(36); // We're making sure the IDs are actually being made.
 	});
 
 	test("Will remove an expense group ID from the state array", () => {
 		state = [ 1, 2, 3, 4 ];
 
-		var action = {
-			type: C.REMOVE_EXPENSE_GROUP,
-			id : 2
-		};
+		var id = 2;
+		var action = removeExpenseGroup({ id });
 
 		var results = expense_groups( state, action );
-
-		expect( results ).toEqual([ 1, 3, 4 ]);
+		expect( results ).toEqual([ 1, 3, 4 ]); // This should be good enough.
 	});
 });
 
@@ -69,14 +82,16 @@ describe("Expense Group", () => {
 	});
 
 	test("Returns state on action not having a type", () => {
-		var action = {
-			id : 0,
-			title : "Expense Group 1",
-			timestamp : new Date().toString()
-		};
+		// var action = {
+		// 	id : 0,
+		// 	title : "Expense Group 1",
+		// 	timestamp : new Date().toString()
+		// };
+
+		var action = addExpenseGroup({ title : "A Test Expense Group" });
+		action.type = undefined;
 
 		var results = expense_group_by_id( state, action );
-
 		expect( results ).toEqual({});
 	});
 
@@ -106,13 +121,13 @@ describe("Expense Group", () => {
 			}
 		};
 
-		var action = {
-			type : C.REMOVE_EXPENSE_GROUP,
-			id : 0
-		};
+		// var action = {
+		// 	type : C.REMOVE_EXPENSE_GROUP,
+		// 	id : 0
+		// };
+		var action = removeExpenseGroup({ id : 0 });
 
 		var results = expense_group_by_id( state, action );
-
 		expect( results ).toEqual({});
 	});
 });
@@ -125,12 +140,10 @@ describe("Expense GroupChildren", () => {
 	});
 
 	test("Will return initial state if there is no type given.", () => {
-		var action = {
-			id : 0
-		};
+		var action = removeExpenseGroupChild({ id : 0 });
+		action.type = undefined;
 
 		var results = expense_group_children( state, action );
-
 		expect( results ).toEqual([]);
 	});
 
@@ -139,6 +152,8 @@ describe("Expense GroupChildren", () => {
 			type : C.ADD_EXPENSE_GROUP_CHILD,
 			id : 0
 		};
+
+
 
 		var results = expense_group_children( state, action );
 
