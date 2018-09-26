d9ffa8384ec7bd12df3d8a665bfc90559ff63460
Spraynard <kellan.martin@gmail.com>
2018-09-25 23:45:48 -0400

    starting to build the app

--- src/components/Stats_Window.js
+++ src/components/Stats_Window.js
@@ -1,8 +1,17 @@
-const React = require('react');
+const React = require("react");
 /**
  * Pure functions only please!
  */
-const Stats_Window = () =>
-	<div>Stats_Window</div>;
+const Stats_Window = ( props, { store } ) => {
+	let stateObject = store.getState();
+	console.log( stateObject );
+
+	return (
+		<div className="stats-window">
+			Stats_Window
+			You have
+		</div>
+	);
+};
 
 module.exports = Stats_Window;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { store } from "./store";
 
-const { render } = ReactDOM;
 const Business_Calculator = require("./components/Business_Calculator");
 
-render(
-	<Business_Calculator store={ store }/>,
-	document.getElementById("app-mount")
-);
+const render = () =>
+	ReactDOM.render(
+		<Business_Calculator store={ store }/>,
+		document.getElementById("app-mount")
+	);
+
+store.subscribe(render);
+render();
+
