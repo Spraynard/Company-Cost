8e77d130a9f9a9a41db2916da4d9fb4220988b08
Spraynard <kellan.martin@gmail.com>
2019-01-03 23:00:43 -0500

    Creating Expense_Group_Child_Default_View and also updating expense_Group_child_edit_view

---
+++ src/components/Expense_Group_Child_Default_View.js
@@ -0,0 +1,36 @@
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import CloseIcon from "@material-ui/icons/Close";
+
+const Expense_Group_Child_Default_View = props => {
+	const {
+		id,
+		childStateData,
+		childDisplayData,
+		childClickHandler,
+		childRemoveHandler
+	} = props;
+
+	return (
+		<TableRow className="expense-group-child" data-id={id} onClick={() => childClickHandler( id, childStateData.edit )}>
+			{
+			// Default UI Display
+				Object.keys(childDisplayData).map(
+					( childDataKey, index ) => (
+						<TableCell key={index} padding="none">
+							{ ( childDataKey === "delete" ) ?
+								<CloseIcon
+									onClick={( event ) => childRemoveHandler( id, childStateData.parentID, event )}
+								/>
+								:
+								childDisplayData[childDataKey]
+							}
+						</TableCell>
+					)
+				)
+			}
+		</TableRow>
+	);
+};
+
+export default Expense_Group_Child_Default_View;
--- src/components/Expense_Group_Child_Edit_View.js
+++ src/components/Expense_Group_Child_Edit_View.js
@@ -1,36 +1,40 @@
 import TableCell from "@material-ui/core/TableCell";
 import InputBase from "@material-ui/core/InputBase";
-
+import TableRow from "@material-ui/core/TableRow";
 const Expense_Group_Child_Edit_View =  ( props ) => {
 	const {
 		id,
+		childDisplayData,
 		childStateData,
 		childEditStateData,
-		childDataChangeHandler
+		childDataChangeHandler,
+		childClickHandler,
 	} = props;
 
 	return (
-		// Edit Mode Display
-		Object.keys( childStateData ).map(
-			( childDataKey, index ) => (
-				<TableCell
-					key={index}
-					padding="none"
-					colSpan={
-						(
-							Object.keys(childStateData).length < 4 &&
-							Object.keys(childStateData).indexOf(childDataKey) === Object.keys(childStateData).length - 1
-						) ? 2 : 1
-					}
-				>
-					<InputBase
-						name={childDataKey}
-						value={childEditStateData[childDataKey]}
-						onChange={( event ) => childDataChangeHandler( id, event )}
-					/>
-				</TableCell>
-			)
-		)
+		<TableRow className="expense-group-child" data-id={id} onClick={() => childClickHandler( id, childStateData.edit )}>
+			{// Edit Mode Display
+				Object.keys( childDisplayData ).map(
+					( childDataKey, index ) => (
+						<TableCell
+							key={index}
+							padding="none"
+							colSpan={
+								(
+									Object.keys(childDisplayData).length < 4 &&
+							Object.keys(childDisplayData).indexOf(childDataKey) === Object.keys(childDisplayData).length - 1
+								) ? 2 : 1
+							}
+						>
+							<InputBase
+								name={childDataKey}
+								value={childEditStateData[childDataKey]}
+								onChange={( event ) => childDataChangeHandler( id, event )}
+							/>
+						</TableCell>
+					)
+				)}
+		</TableRow>
 	);
 };
 
