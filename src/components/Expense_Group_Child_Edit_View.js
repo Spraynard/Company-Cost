615668eeed5fc3cfa998a2d74909d8479d0eb020
Spraynard <kellan.martin@gmail.com>
2019-01-04 19:22:33 -0500

    Edit views now have rudimentary input types in them which allow for more control over the kinds of inputs we are asking of the consumer

--- src/components/Expense_Group_Child_Edit_View.js
+++ src/components/Expense_Group_Child_Edit_View.js
@@ -1,6 +1,9 @@
 import TableCell from "@material-ui/core/TableCell";
 import InputBase from "@material-ui/core/InputBase";
 import TableRow from "@material-ui/core/TableRow";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+
 const Expense_Group_Child_Edit_View =  ( props ) => {
 	const {
 		id,
@@ -11,29 +14,67 @@ const Expense_Group_Child_Edit_View =  ( props ) => {
 		childClickHandler,
 	} = props;
 
+	const editValueTypes = {
+		title : "text",
+		description : "textarea",
+		cost : "number",
+		costUOM : "select",
+	};
+
+	const selectValues = {
+		"costUOM" : [
+			"day",
+			"hour",
+			"week",
+			"month",
+			"year"
+		]
+	};
+
+	// Take child Display data and make an object that contains the data with each of the different types
+	// that are required in order to modify said data.
+
+	// First Move all the current data to
+	console.log( "Child State Data", childStateData );
+	console.log( "Child Display Data", childDisplayData );
+	console.log( "Child Edit State Data", childEditStateData );
+
 	return (
 		<TableRow className="expense-group-child" data-id={id} onClick={() => childClickHandler( id, childStateData.edit )}>
-			{// Edit Mode Display
-				Object.keys( childDisplayData ).map(
-					( childDataKey, index ) => (
+			{Object.keys( childEditStateData ).map(
+				( childDataKey, index ) => {
+					return (
 						<TableCell
 							key={index}
 							padding="none"
 							colSpan={
 								(
-									Object.keys(childDisplayData).length < 4 &&
-							Object.keys(childDisplayData).indexOf(childDataKey) === Object.keys(childDisplayData).length - 1
+									Object.keys(childEditStateData).length < 4 &&
+							Object.keys(childEditStateData).indexOf(childDataKey) === Object.keys(childEditStateData).length - 1
 								) ? 2 : 1
 							}
 						>
-							<InputBase
-								name={childDataKey}
-								value={childEditStateData[childDataKey]}
-								onChange={( event ) => childDataChangeHandler( id, event )}
-							/>
+							{ ( editValueTypes[childDataKey] === "select" ) ?
+								<Select
+									value={childEditStateData[childDataKey]}
+									name={childDataKey}
+									onChange={( event ) => childDataChangeHandler( id, event )}
+								>
+									{ selectValues[childDataKey].map((item, index) =>
+										<MenuItem key={index} value={item}>{item}</MenuItem> )}
+								</Select>
+								:
+								<InputBase
+									name={childDataKey}
+									value={childEditStateData[childDataKey]}
+									type={editValueTypes[childDataKey]}
+									onChange={( event ) => childDataChangeHandler( id, event )}
+								/>
+							}
 						</TableCell>
-					)
-				)}
+					);
+				}
+			)}
 		</TableRow>
 	);
 };
