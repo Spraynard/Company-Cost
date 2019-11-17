0b9f0856edf0127f92103278aedc6e3f7a752f50
Spraynard <kellan.martin@gmail.com>
2019-11-17 03:51:35 -0500

    Adding Open_Status_Drawer_Button component

---
+++ src/components/Buttons/Open_Stats_Drawer_Button.js
@@ -0,0 +1,13 @@
+
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import Menu from "@material-ui/icons/Menu";
+
+const Open_Stats_Drawer_Button = ({ onClick, ...other }) =>
+    <Box { ...other }>
+        <Button>
+            <Menu />
+        </Button>
+    </Box>
+
+export default Open_Stats_Drawer_Button;
