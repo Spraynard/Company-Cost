6896d7a600a4f6d97887946a34e814f47426f154
Spraynard <kellan.martin@gmail.com>
2019-11-19 16:36:04 -0500

    Update Top_App_Bar.js
    Removing unneeded items from component

--- src/components/Top_App_Bar.js
+++ src/components/Top_App_Bar.js
@@ -2,26 +2,20 @@
 import { PropTypes } from "prop-types";
 
 // Actions
-import { addExpenseGroup } from "../actions/expense_group_actions"
 import { openAppOptionsDialog } from "../actions/application_actions";
 import { openMainMenu } from "../actions/user_interface_actions";
 
 // UI Components
-import Stats_Window from "./Stats_Windows/Stats_Window";
 import App_Bar_Stats_Window from "./Stats_Windows/App_Bar_Stats_Window";
 import Open_Stats_Drawer_Button from "./Buttons/Open_Stats_Drawer_Button";
 
 // Material UI
 import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 
 // Material UI Styles Import
 import { withStyles } from "@material-ui/core/styles";
-
-// Material UI Icons
-import AddCircle from "@material-ui/icons/AddCircle";
 
 // Component Styles
 const styles = theme => ({
@@ -63,21 +57,6 @@ const Top_App_Bar = ( props, { store } ) => {
 				>
 				Company Cost
 				</Typography>
-				<Button
-					color='secondary'
-					variant='contained'
-					size='large'
-					className={`${classes.appButtonMargin} ${classes.marginLeft}`}
-					onClick={() => store.dispatch(addExpenseGroup({
-						"title" : "Expense Group"
-					}))}
-				>
-					<AddCircle className={classes.buttonIconStyles}/>
-					<Typography
-						color="inherit"
-						variant="button"
-					>Add Expense Group</Typography>
-				</Button>
 				<Open_Stats_Drawer_Button
 					display={{ xs : 'block', 'md' : 'none' }}
 					onClick={() => store.dispatch(openMainMenu())}
