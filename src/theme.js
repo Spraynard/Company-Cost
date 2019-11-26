d3e1662b53ed69dd7217bfac61a9f7dbff63d27d
Spraynard <kellan.martin@gmail.com>
2019-11-26 14:45:44 -0500

    Updating primary and secondary palette in theme.js

--- src/theme.js
+++ src/theme.js
@@ -10,21 +10,15 @@ export const myTheme = createMuiTheme({
 	},
 	palette: {
 		primary: {
-			light : "#6abf69",
-			dark : "#00600f",
-			main : "#388e3c",
-			contrastText: "#000"
+			main: "#185a9d",
 		},
 		secondary: {
-			light : "#718792",
-			dark : "#1c313a",
 			main : "#455a64",
-			contrastText: "#fff"
 		},
 		gradients: {
-			secondRiver: 'linear-gradient(to left, #43cea2, #185a9d)',
-			secondRiverReverse: 'linear-gradient(to right, #43cea2, #185a9d)',
-			secondRiverToTop: 'linear-gradient(to top, #43cea2, #185a9d)'
+			secondRiver: "linear-gradient(to left, #43cea2, #185a9d)",
+			secondRiverReverse: "linear-gradient(to right, #43cea2, #185a9d)",
+			secondRiverToTop: "linear-gradient(to top, #43cea2, #185a9d)"
 		},
 	},
 	overrides : {
