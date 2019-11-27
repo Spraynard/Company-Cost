7be701833fe6afaacf81b7d23780748a11607637
Spraynard <kellan.martin@gmail.com>
2019-11-27 03:26:15 -0500

    Updating theme

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,19 @@
 import { createMuiTheme } from "@material-ui/core/styles";
+import { lighten } from "@material-ui/core/styles/colorManipulator";
 import "./fonts/fonts.css";
 
-export const myTheme = createMuiTheme({
+const defaultTheme = createMuiTheme({
+	palette : {
+		primary: {
+			main: "#185a9d",
+		},
+		secondary: {
+			main: "#455a64",
+		}
+	}
+});
+
+export const CompanyCostTheme = createMuiTheme({
 	typography : {
 		useNextVariants : true,
 		siteTitle : {
@@ -9,12 +21,7 @@ export const myTheme = createMuiTheme({
 		}
 	},
 	palette: {
-		primary: {
-			main: "#185a9d",
-		},
-		secondary: {
-			main : "#455a64",
-		},
+		...defaultTheme.palette,
 		gradients: {
 			secondRiver: "linear-gradient(to left, #43cea2, #185a9d)",
 			secondRiverReverse: "linear-gradient(to right, #43cea2, #185a9d)",
@@ -22,20 +29,50 @@ export const myTheme = createMuiTheme({
 		},
 	},
 	overrides : {
-		MuiTableRow : {
-			root : {
-				height : 24
-			},
-			head : {
-				height : 32
-			},
-			footer : {
-				height : 32
+		MuiButton: {
+			containedPrimary: {
+				color: "#fff"
 			}
 		},
-		MuiButton : {
-			containedPrimary : {
-				color: "#fff"
+		MuiTableCell : {
+			head : {
+				borderColor: defaultTheme.palette.primary.main,
+				color: defaultTheme.palette.primary.main,
+				paddingBottom : 0,
+				paddingLeft: 0,
+				paddingTop: defaultTheme.spacing(1)
+			},
+			footer : {
+				borderBottom : "none",
+			},
+			sizeSmall : {
+				padding: "0 0 0 4px"
+			}
+		},
+		MuiTableHead: {
+			root : {
+				backgroundColor: lighten( defaultTheme.palette.primary.light, .75 )
+			}
+		},
+		MuiTableRow: {
+			root: {
+				height: 24,
+			},
+			head: {
+				height: 32
+			},
+			footer: {
+				height: 32,
+			}
+		},
+		MuiTableFooter: {
+			root: {
+				transition: defaultTheme.transitions.create(["opacity"])
+			}
+		},
+		MuiTypography: {
+			subtitle2: {
+				fontWeight : defaultTheme.typography.fontWeightLight
 			}
 		}
 	}
