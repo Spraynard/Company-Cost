2c2b73c847ceff27ab69480a847e483ea97de4fc
Spraynard <kellan.martin@gmail.com>
2019-11-28 01:26:14 -0500

    Updating Groups_Window.js
    Formatting

--- src/components/Groups_Window.js
+++ src/components/Groups_Window.js
@@ -2,9 +2,6 @@
 import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 
-// Custom Imports
-import Add_Expense_Group_UI_Button from "./Buttons/Add_Expense_Group_UI_Button";
-
 const styles = theme => ({
 	root: {
 		padding: theme.spacing(3),
@@ -23,7 +20,7 @@ const styles = theme => ({
  * @param addNew - Component used to add a new expense group to the list.
  * @param props - Rest of passed in props.
  */
-const Groups_Window = ({ classes, children, ...props}) => {
+const Groups_Window = ({ classes, children }) => {
 
 	const grid_elements = children.map( ( child, index ) =>
 		<Grid item key={`groups-grid-elem-${index}`} xs={12} md={6} lg={4}>{child}</Grid>
