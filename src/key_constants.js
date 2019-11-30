36dced14961e7def34d2c24b39be44d9c4348eb4
Spraynard <kellan.martin@gmail.com>
2019-11-29 22:58:32 -0500

    Adding in key_constants.js
    Contains named constants for keyboard actions

---
+++ src/key_constants.js
@@ -0,0 +1,16 @@
+/**
+ * Constants used for defining keyboard actions.
+ * Thought it would be a little more readable then just saying
+ * e.which === 32
+ */
+const key_constants = {
+	"BACKSPACE_KEY" : 8,
+	"DELETE_KEY" : 46,
+	"ENTER_KEY" : 13,
+	"ESCAPE_KEY" : 27,
+	"SHIFT_KEY" : 16,
+	"SPACE_KEY" : 32,
+	"TAB_KEY" : 9,
+};
+
+module.exports = key_constants;
